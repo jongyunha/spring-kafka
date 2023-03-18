7dac7092fc9050c651680b2b0713f843b28661d7
jy <70758906+jongyunha@users.noreply.github.com>
2023-03-18 14:47:04 +0900

    Build the library event domain (#1)

---
+++ src/main/kotlin/com/example/springkafka/domain/Book.kt
@@ -0,0 +1,7 @@
+package com.example.springkafka.domain
+
+data class Book(
+    private val id: Int,
+    private val name: String,
+    private val author: String,
+)
--- src/test/kotlin/com/example/springkafka/SpringKafkaApplicationTests.kt
+++ src/test/kotlin/com/example/springkafka/SpringKafkaApplicationTests.kt
@@ -9,5 +9,4 @@ class SpringKafkaApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
